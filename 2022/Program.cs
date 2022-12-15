100089c2a5cf8f84b61e382e71453b5f7f8cbe6c
The Milk Man <ajayseanx@gmail.com>
2022-12-15 22:02:33 +0000

    Day 10

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using AdventOfCode.Puzzle;
 
-var day = new Day9();
+var day = new Day10();
 Console.WriteLine($"Part 1: {Profile(day.Part1)}ms");
 Console.WriteLine();
 Console.WriteLine($"Part 2: {Profile(day.Part2)}ms");
---
+++ 2022/Puzzle/Day10.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode.Puzzle;
+
+public class Day10 : IDay
+{
+    public void Part1() => Console.WriteLine(SignalStrength());
+
+    public void Part2() => Console.WriteLine(RenderSignal());
+
+    private int SignalStrength()
+    {
+        var operations = IDay.ReadResource(10);
+        var significantCycles = new List<int>() { 20, 60, 100, 140, 180, 220 };
+        var x = 1;
+        var cycle = 0;
+        var result = 0;
+        foreach (var operation in operations)
+        {
+            cycle++;
+            if (significantCycles.Contains(cycle)) result += x*cycle;
+            if (operation.StartsWith("addx "))
+            {
+                cycle++;
+                if (significantCycles.Contains(cycle)) result += x*cycle;
+                x += int.Parse(operation.Substring("addx ".Length));
+            }
+        }
+        return result;
+    }
+
+    private string RenderSignal()
+    {
+        var operations = IDay.ReadResource(10);
+        var x = 1;
+        var cycle = 0;
+        var result = "";
+        foreach (var operation in operations)
+        {
+            cycle++;
+            var position = (cycle - 1) % 40;
+            result += x-1 <= position && position <= x+1 ? "#" : ".";
+            if (operation.StartsWith("addx "))
+            {
+                cycle++;
+                position = (cycle - 1) % 40;
+                result += x-1 <= position && position <= x+1 ? "#" : ".";
+                x += int.Parse(operation.Substring("addx ".Length));
+            }
+        }
+        return string.Join('\n', result.Chunk(40).Take(6).Select(l => string.Concat(l)));
+    }
+}
