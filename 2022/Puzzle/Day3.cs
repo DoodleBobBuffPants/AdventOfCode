1d8e9258265016ccf179084dde1495d6e5f6c76f
The Milk Man <ajayseanx@gmail.com>
2022-12-15 22:01:36 +0000

    Day 3

---
+++ 2022/Puzzle/Day3.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Puzzle;
+
+public class Day3 : IDay
+{
+    public void Part1() => Console.WriteLine(GetPrioritySum(GetDuplicates()));
+
+    public void Part2() => Console.WriteLine(GetPrioritySum(GetBadges()));
+
+    private int GetPrioritySum(IEnumerable<IEnumerable<char>> source) =>
+        source.Select(x => x.First())
+              .Select(x => char.IsUpper(x) ? x - 'A' + 27 : x - 'a' + 1)
+              .Sum();
+
+    private IEnumerable<IEnumerable<char>> GetDuplicates() =>
+        IDay.ReadResource(3)
+            .Select(x => x.Substring(0, x.Length/2).Intersect(x.Substring(x.Length/2)));
+
+    private IEnumerable<IEnumerable<char>> GetBadges() =>
+        IDay.ReadResource(3)
+            .Chunk(3)
+            .Select(x => x[0].Intersect(x[1]).Intersect(x[2]));
+}
