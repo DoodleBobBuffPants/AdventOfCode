50df506f9c4e829453cd1a4b79de1fcd3a299e92
The Milk Man <ajayseanx@gmail.com>
2022-12-15 22:00:58 +0000

    Day 1

--- 2022/Puzzle/Day1.cs
+++ 2022/Puzzle/Day1.cs
@@ -2,7 +2,28 @@
 
 public class Day1 : IDay
 {
-    public void Part1() => Console.WriteLine();
+    public void Part1() => Console.WriteLine(GetCaloryCounts().Max);
 
-    public void Part2() => Console.WriteLine();
+    public void Part2() => Console.WriteLine(GetCaloryCounts().Sum());
+
+    private SortedSet<long> GetCaloryCounts()
+    {
+        var lines = IDay.ReadResource(1);
+        var counts = new SortedSet<long>();
+        var current = 0L;
+        foreach(var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                counts.Add(current);
+                if (counts.Count() > 3) counts.Remove(counts.Min);
+                current = 0;
+            }
+            else
+            {
+                current += long.Parse(line);
+            }
+        }
+        return counts;
+    }
 }
