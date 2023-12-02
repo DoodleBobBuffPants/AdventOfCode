2caae304364c721dea21f6677a3ea36ea9f625dd
The Milk Man <ajayseanx@gmail.com>
2023-12-02 15:04:42 +0000

    Day 1

--- 2023/src/main/java/puzzle/Day1.java
+++ 2023/src/main/java/puzzle/Day1.java
@@ -3,11 +3,19 @@ package puzzle;
 public class Day1 implements Day {
     @Override
     public void part1() {
-        
+        System.out.println(readResource(1).stream().mapToLong(l -> Long.parseLong(firstDigit(l) + lastDigit(l))).sum());
     }
     
     @Override
     public void part2() {
         
+    }
+
+    private String firstDigit(String text) {
+        return Character.toString(text.chars().dropWhile(c -> !Character.isDigit(c)).findFirst().orElseThrow());
+    }
+
+    private String lastDigit(String text) {
+        return firstDigit(new StringBuilder(text).reverse().toString());
     }
 }
